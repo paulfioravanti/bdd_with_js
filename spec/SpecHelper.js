fb1361d76199d1d9722fbfd811ad8bd01208168d
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-06-01 14:22:59 +1000

    Clean up

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -26,8 +26,8 @@ beforeEach(function() {
             pass: actual.has3LetterDiscountGranted(),
             message: "Expected the seminar '" + actual + "' to " +
               "return 'true' on #has3LetterDiscountGranted but got '" +
-              (actual.has3LetterDiscountGranted &&
-                actual.has3LetterDiscountGranted()) + "'"
+              actual.has3LetterDiscountGranted &&
+                actual.has3LetterDiscountGranted() + "'"
           }
         },
         negativeCompare: function(actual, expected) {
@@ -35,8 +35,8 @@ beforeEach(function() {
             pass: !actual.has3LetterDiscountGranted(),
             message: "Expected the seminar '" + actual + "' to " +
               "return 'false' on #has3LetterDiscountGranted but got '" +
-              (actual.has3LetterDiscountGranted &&
-                !actual.has3LetterDiscountGranted()) + "'"
+              actual.has3LetterDiscountGranted &&
+                !actual.has3LetterDiscountGranted() + "'"
           }
         }
       };
